8d434aba0394de41ecdd32169f074ee525010175
jeffreysalewis <jeffreysalewis@gmail.com>
2024-01-13 14:28:39 -0600

    changed notimplementedexception to invalidmoveexception

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -59,7 +59,7 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        throw new RuntimeException("Not implemented");
+        throw new InvalidMoveException("Invalid move");
     }
 
     /**
