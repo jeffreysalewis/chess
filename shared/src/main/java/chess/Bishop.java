5b265e5b8463494685eab8c2baacff9f779ba6bf
jeffreysalewis <jeffreysalewis@gmail.com>
2024-01-20 17:55:33 -0600

    had Bishop override pieceMoves & return a HashSet w/ different filler val

--- shared/src/main/java/chess/Bishop.java
+++ shared/src/main/java/chess/Bishop.java
@@ -1,9 +1,18 @@
 package chess;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 public class Bishop extends ChessMove {
     private ChessPiece.PieceType type = ChessPiece.PieceType.BISHOP;
     public Bishop(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
         super(startPosition, endPosition, promotionPiece);
         this.type = ChessPiece.PieceType.BISHOP;
+    }
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        HashSet<ChessMove> mv = new HashSet<ChessMove>();
+        mv.add(new ChessMove(new ChessPosition(0, 0), new ChessPosition(2, 4), ChessPiece.PieceType.BISHOP));
+        return mv;
     }
 }
