be7efe2d8b4f3fa1b9e016e509b6598a228bacd1
jeffreysalewis <jeffreysalewis@gmail.com>
2024-01-20 17:53:23 -0600

    had pieceMoves return a HashSet and put a filler val

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Represents a single chess piece
@@ -52,6 +53,8 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
+        HashSet<ChessMove> mv = new HashSet<ChessMove>();
+        mv.add(new ChessMove(new ChessPosition(0, 0), new ChessPosition(1, 3), ChessPiece.PieceType.BISHOP));
+        return mv;
     }
 }
