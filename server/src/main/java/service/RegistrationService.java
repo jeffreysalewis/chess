ffe107f84bd8e6e1fa9adb700f5b6b27389f891a
jeffreysalewis <jeffreysalewis@gmail.com>
2024-02-27 13:57:59 -0700

    added constructor and getter for RegistrationService

--- server/src/main/java/service/RegistrationService.java
+++ server/src/main/java/service/RegistrationService.java
@@ -1,4 +1,18 @@
 package service;
 
 public class RegistrationService {
+    private String username;
+    private String password;
+    private String email;
+    private String authtoken;
+    public RegistrationService(String usnm, String pwd, String cor) {
+        this.username = usnm;
+        this.password = pwd;
+        this.email = cor;
+        this.authtoken = this.email + " correo";
+    }
+
+    public String getauthtoken() {
+        return this.authtoken;
+    }
 }
