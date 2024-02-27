3adc2f3011f167263ca25bc850dc624fae1a1343
jeffreysalewis <jeffreysalewis@gmail.com>
2024-02-27 13:57:19 -0700

    added register endpoint api

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,8 @@
 package server;
+import com.google.gson.Gson;
 import spark.*;
 import exception.*;
+import service.*;
 
 public class Server {
     public int run(int desiredPort) {
@@ -9,6 +11,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/chess", this::register);
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -24,5 +27,12 @@ public class Server {
 
     private void exceptionHandler(ResponseException ex, Request req, Response res) {
         res.status(ex.StatusCode());
+    }
+
+    private Object register(Request req, Response res) throws ResponseException {
+        var user = new Gson().fromJson(req.body(), RegistrationService.class);
+        res.type("application/json");
+        var authtoken = user.getauthtoken();
+        return new Gson().toJson(authtoken);
     }
 }
