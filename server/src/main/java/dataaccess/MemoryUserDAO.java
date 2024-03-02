f40b0e34975b078c157c6c78cd89052d744e4f87
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-02 14:58:31 -0700

    back to working, deleted static var

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -5,20 +5,20 @@ import java.util.*;
 public class MemoryUserDAO implements UserDAO{
     private String username;
     private String authtoken;
-    private static HashMap<String, String[]> userdata = new HashMap<>();
+    private HashMap<String, String[]> userdata = new HashMap<>();
     public MemoryUserDAO() {
     }
 
     @Override
     public String[] getUser(String username) {
-        if (MemoryUserDAO.userdata == null) {
+        if (userdata == null) {
             return null;
         }
-        return MemoryUserDAO.userdata.get(username);
+        return userdata.get(username);
     }
 
     public void createUser(String username, String password, String email) {
         var ac = new String[]{username, password, email};
-        MemoryUserDAO.userdata.put(username, ac);
+        userdata.put(username, ac);
     }
 }
