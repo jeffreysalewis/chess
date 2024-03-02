e9945cadd3fbaf87e31cc6fc62459e7837675361
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-02 14:34:13 -0700

    finished writing basic tests for RegistrationService

--- server/src/test/java/servicesTests/RegistrationServiceTest.java
+++ server/src/test/java/servicesTests/RegistrationServiceTest.java
@@ -1,11 +1,18 @@
 package servicesTests;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
+import service.RegistrationService;
 
 public class RegistrationServiceTest {
     @Test
     @DisplayName("RegistrationService test ;p")
-    public void registrationServiceTest() {
-
+    public void registrationServiceTest() throws Exception {
+        var user = new RegistrationService("ausername", "apassword", "anemail");
+        System.out.println(user.registerUser());
+        System.out.println(user.registerUser());
+        Assertions.assertTrue("ausername".equals(user.getusername()));
+        Assertions.assertTrue("apassword".equals(user.getpassword()));
+        Assertions.assertTrue("anemail".equals(user.getemail()));
     }
 }
