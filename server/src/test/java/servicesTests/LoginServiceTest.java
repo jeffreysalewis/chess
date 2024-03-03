187d2f14ca6ee35e4569f9a87d725cb6510ca227
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-03 00:58:15 -0700

    added vars to LoginServiceTest

--- server/src/test/java/servicesTests/LoginServiceTest.java
+++ server/src/test/java/servicesTests/LoginServiceTest.java
@@ -1,9 +1,12 @@
 package servicesTests;
 import org.junit.jupiter.api.*;
+import service.*;
+
 public class LoginServiceTest {
     @Test
     @DisplayName("LoginServiceService test ;p")
     public void loginServiceTest() {
-
+        var user = new RegistrationService("ausername", "apassword", "anemail");
+        var log = new LoginService(user.getemail(), user.getpassword());
     }
 }
