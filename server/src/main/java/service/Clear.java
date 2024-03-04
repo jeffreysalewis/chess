0c8e2e20bb29b09541dfe846781f89d27e438f4a
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-04 04:47:51 -0700

    fixed clear endpoint not calling to db

--- server/src/main/java/service/Clear.java
+++ server/src/main/java/service/Clear.java
@@ -1,11 +1,15 @@
 package service;
 
+import dataaccess.*;
+
 public class Clear {
     public Clear() {
 
     }
 
     public void clear() {
-
+        MemoryUserDAO.clear();
+        MemoryAuthDAO.clear();
+        MemoryGameDAO.clear();
     }
 }
