e3ec8926030d453ae4ad9fe64850f3d66d0b8d1f
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-07 01:06:13 -0700

    updated gamedao interface

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,11 @@
 package dataaccess;
 
+import exception.ResponseException;
+
+import java.util.Map;
+
 public interface GameDAO {
+    public int nueva(String name);
+    public void join(String color, int id, String username) throws ResponseException;
+    public Map<String, String>[] list();
 }
