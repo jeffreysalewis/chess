3d2fe1f145f4db584090ba172427003f11348490
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-09 22:10:43 -0700

    game sql dataaccess passes custom tests and works

--- server/src/main/java/dataAccess/SqlGameDAO.java
+++ server/src/main/java/dataAccess/SqlGameDAO.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import exception.ResponseException;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 import chess.*;
 
@@ -18,7 +19,8 @@ public class SqlGameDAO implements GameDAO{
             this.configureDatabase();
             try (var conn = DatabaseManager.getConnection()) {
                 try (var preparedStatement = conn.prepareStatement("INSERT INTO game (gameID, whiteUsername, blackUsername, gameName, gamejson) VALUES(?, ?, ?, ?, ?)")) {
-                    preparedStatement.setInt(1, 0);
+                    int gamid = (int) (Math.random()*1000000);
+                    preparedStatement.setInt(1, gamid);
                     preparedStatement.setString(2, "");
                     preparedStatement.setString(3, "");
                     preparedStatement.setString(4, name);
@@ -26,6 +28,7 @@ public class SqlGameDAO implements GameDAO{
                     preparedStatement.setString(5, gamejson);
 
                     preparedStatement.executeUpdate();
+                    return gamid;
                 } catch (Exception e) {
                     throw new ResponseException(500, String.format("Unable to create authtoken: %s", e.getMessage()));
                 }
@@ -35,17 +38,103 @@ public class SqlGameDAO implements GameDAO{
         } catch (Exception e) {
             throw new ResponseException(500, String.format("Unable to create authtoken: %s", e.getMessage()));
         }
-        return 0;
     }
 
     @Override
     public void join(String color, int id, String username) throws ResponseException {
+        try {
+            this.configureDatabase();
+            try (var conn = DatabaseManager.getConnection()) {
+                String updatestr;
+                if(color == "WHITE") {
+                    updatestr = "UPDATE game SET whiteUsername=? WHERE gameID=?";
+                } else if(color == "BLACK") {
+                    updatestr = "UPDATE game SET blackUsername=? WHERE gameID=?";
+                } else {
+                    try (var preparedStatement = conn.prepareStatement(" SELECT gameID, whiteUsername, blackUsername, gameName, gamejson FROM game WHERE gameID=?")) {
+                        preparedStatement.setInt(1, id);
+                        var rs = preparedStatement.executeQuery();
+                        rs.next();
+                        var gid = rs.getInt("gameID");
+                    } catch (Exception e) {
+                        throw new ResponseException(500, String.format("Unable to join: %s", e.getMessage()));
+                    }
+                    return;
+                }
+                try (var preparedStatement = conn.prepareStatement(updatestr)) {
+                    preparedStatement.setString(1, username);
+                    preparedStatement.setInt(2, id);
 
+                    preparedStatement.executeUpdate();
+                } catch (Exception e) {
+                    throw new ResponseException(500, String.format("Unable to join: %s", e.getMessage()));
+                }
+            } catch (Exception e) {
+                throw new ResponseException(500, String.format("Unable to join: %s", e.getMessage()));
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to create authtoken: %s", e.getMessage()));
+        }
     }
 
     @Override
-    public Map<String, String>[] list() {
-        return new Map[0];
+    public Map<String, String>[] list() throws ResponseException{
+        int len = 0;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT gameID, whiteUsername, blackUsername, gameName, gamejson FROM game")) {
+                try (var rs = preparedStatement.executeQuery()) {
+                    while(rs.next()) {
+                        len++;
+                    }
+                }  catch (Exception e) {
+                    throw new ResponseException(500, String.format("Unable to get user from database: %s", e.getMessage()));
+                }
+            } catch (Exception e) {
+                throw new ResponseException(500, String.format("Unable to get user from database: %s", e.getMessage()));
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to get user from database: %s", e.getMessage()));
+        }
+        var bettergamelist = new HashMap[len];
+        len = 0;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT gameID, whiteUsername, blackUsername, gameName, gamejson FROM game")) {
+                try (var rs = preparedStatement.executeQuery()) {
+                    while(rs.next()) {
+                        var id = rs.getInt("gameID");
+                        var wu = rs.getString("whiteUsername");
+                        var bu = rs.getString("blackUsername");
+                        var na = rs.getString("gameName");
+                        var gj = rs.getString("gamejson");
+                        bettergamelist[len] = new HashMap<String, String>();
+                        bettergamelist[len].put("gameID", id);
+                        bettergamelist[len].put("whiteUsername", wu);
+                        bettergamelist[len].put("blackUsername", bu);
+                        bettergamelist[len].put("gameName", na);
+                        len++;
+                    }
+                }  catch (Exception e) {
+                    throw new ResponseException(500, String.format("Unable to get user from database: %s", e.getMessage()));
+                }
+            } catch (Exception e) {
+                throw new ResponseException(500, String.format("Unable to get user from database: %s", e.getMessage()));
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to get user from database: %s", e.getMessage()));
+        }
+        return bettergamelist;
+    }
+
+    public static void clear() throws ResponseException{
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("TRUNCATE game")) {
+                preparedStatement.executeUpdate();
+            } catch(Exception e) {
+                throw new ResponseException(500, String.format("Unable to clear auth in database: %s", e.getMessage()));
+            }
+        } catch(Exception r) {
+            throw new ResponseException(500, String.format("Unable to clear auth in database: %s", r.getMessage()));
+        }
     }
 
     private final String[] createStatements = {
@@ -56,7 +145,7 @@ public class SqlGameDAO implements GameDAO{
               `blackUsername` varchar(256) NOT NULL,
               `gameName` varchar(256) NOT NULL,
               `gamejson` text NOT NULL,
-              PRIMARY KEY (`gameID`),
+              PRIMARY KEY (`gameID`)
             )
             """
     };
--- server/src/test/java/dataaccessTests/GameDAOTest.java
+++ server/src/test/java/dataaccessTests/GameDAOTest.java
@@ -1,5 +1,9 @@
 package dataaccessTests;
 
+import dataAccess.SqlAuthDAO;
+import dataAccess.SqlGameDAO;
+import exception.ResponseException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,30 +11,157 @@ public class GameDAOTest {
     @Test
     @DisplayName("nueva() test >:D")
     public void nuevaTest() {
+        try {
+            var newgamedata = new SqlGameDAO();
+            var randganame = "amogus";
+            var outid = newgamedata.nueva(randganame);
+            Assertions.assertNotNull(outid);
+        } catch (ResponseException r) {
+            System.out.println(r.StatusCode());
+            System.out.println(r.getMessage());
+            Assertions.assertTrue(false);
+        } catch (Exception e) {
+            Assertions.fail();
+        }
     }
 
     @Test
     @DisplayName("mal nueva() probar >:D")
     public void malnuevaTest() {
+        try {
+            var newgamedata = new SqlGameDAO();
+            var randganame = "amongus";
+            var outid = newgamedata.nueva(randganame);
+            Assertions.assertEquals(outid, outid);
+        } catch (ResponseException r) {
+            System.out.println(r.StatusCode());
+            System.out.println(r.getMessage());
+            Assertions.assertTrue(false);
+        } catch (Exception e) {
+            Assertions.fail();
+        }
     }
 
     @Test
     @DisplayName("join() test >:D")
     public void joinTest() {
+        try {
+            var newgamedata = new SqlGameDAO();
+            var randganame = "minecraft";
+            var outid = newgamedata.nueva(randganame);
+            newgamedata.join("BLACK", outid, "blackusnm");
+        } catch (ResponseException r) {
+            System.out.println(r.StatusCode());
+            System.out.println(r.getMessage());
+            Assertions.assertTrue(false);
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+        Assertions.assertTrue(true);
     }
 
     @Test
     @DisplayName("bad join() test >:D")
     public void badjoinTest() {
+        try {
+            var newgamedata = new SqlGameDAO();
+            var randganame = "minecraft";
+            var outid = newgamedata.nueva(randganame);
+            newgamedata.join("WHITE", outid, "whiteusnm");
+        } catch (ResponseException r) {
+            System.out.println(r.StatusCode());
+            System.out.println(r.getMessage());
+            Assertions.assertTrue(false);
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+        Assertions.assertTrue(true);
     }
 
     @Test
     @DisplayName("list() test >:D")
     public void listTest() {
+        try {
+            var newgamedata = new SqlGameDAO();
+            var randganame = "undertale";
+            var outid = newgamedata.nueva(randganame);
+            newgamedata.join("WHITE", outid, "birb");
+            newgamedata.join("BLACK", outid, "dawg");
+            newgamedata.join("", outid, "player");
+            var glist = newgamedata.list();
+            Assertions.assertTrue(glist.toString().length() > 16);
+        } catch (ResponseException r) {
+            System.out.println(r.StatusCode());
+            System.out.println(r.getMessage());
+            Assertions.assertTrue(false);
+        } catch (Exception e) {
+            Assertions.fail();
+        }
     }
 
     @Test
     @DisplayName("bad list() test >:D")
     public void badlistTest() {
+        try {
+            var newgamedata = new SqlGameDAO();
+            var randganame = "totk";
+            var outid = newgamedata.nueva(randganame);
+            newgamedata.join("WHITE", outid, "idk");
+            newgamedata.join("BLACK", outid, "smth");
+            var glist = newgamedata.list();
+            Assertions.assertTrue(glist.toString().length() > 15);
+        } catch (ResponseException r) {
+            System.out.println(r.StatusCode());
+            System.out.println(r.getMessage());
+            Assertions.assertTrue(false);
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    @DisplayName("clear() test >:D")
+    public void clearTest() {
+        try {
+            var newgamedata = new SqlGameDAO();
+            var randgamename = "hollow knight";
+            var outid = newgamedata.nueva(randgamename);
+            SqlGameDAO.clear();
+            try{
+                newgamedata.join("", outid, "ghostie");
+                Assertions.fail();
+            } catch (Exception e) {
+                Assertions.assertTrue(true);
+            }
+        } catch (ResponseException r) {
+            System.out.println(r.StatusCode());
+            System.out.println(r.getMessage());
+            Assertions.fail();
+        }catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    @DisplayName("bad clear() test >:D")
+    public void badclearTest() {
+        try {
+            var newgamedata = new SqlGameDAO();
+            var randgamename = "omori";
+            var outid = newgamedata.nueva(randgamename);
+            SqlGameDAO.clear();
+            try{
+                newgamedata.join("", outid, "something");
+                Assertions.fail();
+            } catch (Exception e) {
+                Assertions.assertTrue(true);
+            }
+        } catch (ResponseException r) {
+            System.out.println(r.StatusCode());
+            System.out.println(r.getMessage());
+            Assertions.fail();
+        }catch (Exception e) {
+            Assertions.fail();
+        }
     }
 }
