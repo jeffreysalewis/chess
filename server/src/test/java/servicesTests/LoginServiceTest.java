1dea04fbfc6d4157357576b88af5d15fee385480
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-12 16:07:43 -0600

    added phase 3 tests

--- server/src/test/java/servicesTests/LoginServiceTest.java
+++ server/src/test/java/servicesTests/LoginServiceTest.java
@@ -6,7 +6,7 @@ public class LoginServiceTest {
     @Test
     @DisplayName("LoginServiceService test ;p")
     public void loginServiceTest() throws Exception{
-        var user = new RegistrationService("aus", "apassword", "anemail");
+        var user = new RegistrationService("ausss", "apassword", "anemail");
         var auth = user.registerUser();
         var log = new LoginService(user.getemail(), user.getpassword());
         Assertions.assertTrue(log.getClass() == LoginService.class);
@@ -15,8 +15,26 @@ public class LoginServiceTest {
     @Test
     @DisplayName("bad LoginServiceService test ;p")
     public void badloginServiceTest() throws Exception{
-        var user = new RegistrationService("ause", "apassword", "anemail");
+        var user = new RegistrationService("ausefjdsklasdf", "apassword", "anemail");
         var log = new LoginService(user.getemail(), user.getpassword());
         Assertions.assertFalse(log.getClass() != LoginService.class);
+    }
+
+    @Test
+    @DisplayName("getusername() test ;p")
+    public void getusernameTest() throws Exception {
+        var user = new RegistrationService("jjausername", "apassword", "anemail");
+        var log = new LoginService("jjausername", "apassword");
+        System.out.println(log.getUsername());
+        Assertions.assertTrue("jjausername".equals(log.getUsername()));
+    }
+
+    @Test
+    @DisplayName("bad getusername() test ;p")
+    public void badgetusernameTest() throws Exception {
+        var user = new RegistrationService("jjjausername", "apassword", "anemail");
+        var log = new LoginService("jjjausername", "apassword");
+        System.out.println(log.getUsername());
+        Assertions.assertFalse("a".equals(log.getUsername()));
     }
 }
--- server/src/test/java/servicesTests/RegistrationServiceTest.java
+++ server/src/test/java/servicesTests/RegistrationServiceTest.java
@@ -8,9 +8,9 @@ public class RegistrationServiceTest {
     @Test
     @DisplayName("RegistrationService test ;p")
     public void registrationServiceTest() throws Exception {
-        var user = new RegistrationService("ausername", "apassword", "anemail");
+        var user = new RegistrationService("aaaausername", "apassword", "anemail");
         System.out.println(user.registerUser());
-        Assertions.assertTrue("ausername".equals(user.getusername()));
+        Assertions.assertTrue("aaaausername".equals(user.getusername()));
         Assertions.assertTrue("apassword".equals(user.getpassword()));
         Assertions.assertTrue("anemail".equals(user.getemail()));
     }
@@ -20,5 +20,53 @@ public class RegistrationServiceTest {
     public void badregistrationServiceTest() throws Exception {
         var user = new RegistrationService("acusername", "ausername", "ausername");
         Assertions.assertNotNull(user.registerUser());
+    }
+
+    @Test
+    @DisplayName("getusername() test ;p")
+    public void getusernameTest() throws Exception {
+        var user = new RegistrationService("aausername", "apassword", "anemail");
+        System.out.println(user.getusername());
+        Assertions.assertTrue("aausername".equals(user.getusername()));
+    }
+
+    @Test
+    @DisplayName("badgetusername() test ;p")
+    public void badgetusernameTest() throws Exception {
+        var user = new RegistrationService("ausername", "apassword", "anemail");
+        System.out.println(user.getusername());
+        Assertions.assertFalse("ano".equals(user.getusername()));
+    }
+
+    @Test
+    @DisplayName("getpasswrod() test ;p")
+    public void getpasswordTest() throws Exception {
+        var user = new RegistrationService("ausername", "apassword", "anemail");
+        System.out.println(user.getpassword());
+        Assertions.assertTrue("apassword".equals(user.getpassword()));
+    }
+
+    @Test
+    @DisplayName("badgetpassword() test ;p")
+    public void badgetpasswordTest() throws Exception {
+        var user = new RegistrationService("aasdfdausername", "apassword", "anemail");
+        System.out.println(user.getpassword());
+        Assertions.assertFalse("ap".equals(user.getpassword()));
+    }
+
+    @Test
+    @DisplayName("getemail() test ;p")
+    public void getemailTest() throws Exception {
+        var user = new RegistrationService("accccusername", "apassword", "anemail");
+        System.out.println(user.getemail());
+        Assertions.assertTrue("anemail".equals(user.getemail()));
+    }
+
+    @Test
+    @DisplayName("badgetemail() test ;p")
+    public void badgetemailTest() throws Exception {
+        var user = new RegistrationService("anofdafdusername", "apassword", "anemail");
+        System.out.println(user.getemail());
+        Assertions.assertFalse("ano".equals(user.getemail()));
     }
 }
