6c1b36455fd3cf1f0165dfc062f5cf442d6a3ddb
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-11 23:27:41 -0600

    fixed json not properly inputing gameName in creategame endpoint, persistence tests pass

--- server/src/main/java/service/CreateGameService.java
+++ server/src/main/java/service/CreateGameService.java
@@ -4,16 +4,16 @@ import dataAccess.*;
 import exception.*;
 
 public class CreateGameService {
-    private String name;
-    public CreateGameService(String name) {
-        this.name = name;
+    private String gameName;
+    public CreateGameService(String gameName) {
+        this.gameName = gameName;
     }
 
     public int create(String auth) throws ResponseException {
         try {
             if (SqlAuthDAO.authorize(auth)) {
                 var game = new SqlGameDAO();
-                var gameid = game.nueva(this.name);
+                var gameid = game.nueva(this.gameName);
                 return gameid;
             } else {
                 throw new ResponseException(401, "Error: unauthorized");
