1386f64c53658b2328fadf021a1322ea7dcae07f
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-21 13:12:12 -0600

    added server response and made it the return value

--- client/src/main/java/serverfac/HttpCommunicator.java
+++ client/src/main/java/serverfac/HttpCommunicator.java
@@ -2,12 +2,13 @@ package serverfac;
 
 import com.google.gson.Gson;
 
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.Map;
 
 public class HttpCommunicator {
-    public void run(String path, String reqmeth, String header, String json) throws Exception{
+    public String run(String path, String reqmeth, String header, String json) throws Exception{
         // Specify the desired endpoint
         URI uri = new URI("http://localhost:8080/"+path);
         HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
@@ -32,5 +33,22 @@ public class HttpCommunicator {
                 outputStream.write(json.getBytes());
             }
         }
+
+        if (http.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            // Get HTTP response headers, if necessary
+            // Map<String, List<String>> headers = http.getHeaderFields();
+            // OR
+            //connection.getHeaderField("Content-Length");
+
+            InputStream responseBody = http.getInputStream();
+            // Read and process response body from InputStream ...
+            String res = new String(responseBody.readAllBytes());
+            return res;
+        } else {
+            // SERVER RETURNED AN HTTP ERROR
+            InputStream responseBody = http.getErrorStream();
+            // Read and process error response body from InputStream ...
+        }
+        return null;
     }
 }
