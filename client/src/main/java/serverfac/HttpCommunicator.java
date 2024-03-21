3b71278f3551f8d3c5f09cb8d0bc93ae18ad9117
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-20 23:51:46 -0600

    HttpCommunicator implemented using extensible run method

--- client/src/main/java/serverfac/HttpCommunicator.java
+++ client/src/main/java/serverfac/HttpCommunicator.java
@@ -7,26 +7,26 @@ import java.net.URI;
 import java.util.Map;
 
 public class HttpCommunicator {
-    public void run(String reqmeth, String json) throws Exception{
+    public void run(String path, String reqmeth, String json) throws Exception{
         // Specify the desired endpoint
-        URI uri = new URI("http://localhost:8080/name");
+        URI uri = new URI("http://localhost:8080/"+path);
         HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
-        http.setRequestMethod("GET");
+        http.setRequestMethod(reqmeth);
 
         // Make the request
         http.connect();
 
         // Specify that we are going to write out data
-        http.setDoOutput(true);
+        if(json != null) {
+            http.setDoOutput(true);
 
-        // Write out a header
-        http.addRequestProperty("Content-Type", "application/json");
+            // Write out a header
+            http.addRequestProperty("Content-Type", "application/json");
 
-        // Write out the body
-        var body = Map.of("name", "joe", "type", "cat");
-        try (var outputStream = http.getOutputStream()) {
-            var jsonBody = new Gson().toJson(body);
-            outputStream.write(jsonBody.getBytes());
+            // Write out the body
+            try (var outputStream = http.getOutputStream()) {
+                outputStream.write(json.getBytes());
+            }
         }
     }
 }
