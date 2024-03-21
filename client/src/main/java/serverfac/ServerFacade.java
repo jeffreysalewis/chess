db4c726f7c7271b8bf5bb6d1d930a1308eb6de89
jeffreysalewis <jeffreysalewis@gmail.com>
2024-03-21 14:33:11 -0600

    serverfacade returns  response from http

--- client/src/main/java/serverfac/ServerFacade.java
+++ client/src/main/java/serverfac/ServerFacade.java
@@ -1,5 +1,7 @@
 package serverfac;
 import com.google.gson.Gson;
+import jsonObjClasses.*;
+
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -10,8 +12,9 @@ public class ServerFacade {
         HttpCommunicator httpcon = new HttpCommunicator();
         var body = Map.of("username", username, "password", password, "email", email);
         var jsonBody = new Gson().toJson(body);
-        httpcon.run("user", "POST", null, jsonBody);
-        return "";
+        var retjson = httpcon.run("user", "POST", null, jsonBody);
+        var auth = new Gson().fromJson(retjson, userAuth.class);
+        return auth.getAuthToken();
     }
 
     public String login(String username, String password) throws Exception{
@@ -19,29 +22,30 @@ public class ServerFacade {
         HttpCommunicator httpcon = new HttpCommunicator();
         var body = Map.of("username", username, "password", password);
         var jsonBody = new Gson().toJson(body);
-        httpcon.run("session", "POST", null, jsonBody);
-        return "";
+        var retjson = httpcon.run("session", "POST", null, jsonBody);
+        var auth = new Gson().fromJson(retjson, userAuth.class);
+        return auth.getAuthToken();
     }
 
     public void logout(String authToken) throws Exception{
         HttpCommunicator httpcon = new HttpCommunicator();
-        var body = Map.of("authToken", authToken);
-        var jsonBody = new Gson().toJson(body);
-        httpcon.run("session", "DELETE", authToken, jsonBody);
+        httpcon.run("session", "DELETE", authToken, null);
     }
 
     public Map<String, String>[] listgames(String authToken) throws Exception{
         HttpCommunicator httpcon = new HttpCommunicator();
-        httpcon.run("game", "GET", authToken, null);
-        return null;
+        var retjson = httpcon.run("game", "GET", authToken, null);
+        var lista = new Gson().fromJson(retjson, listaDeJuegos.class);
+        return lista.getGames();
     }
 
     public int creategame(String authToken, String gameName) throws Exception{
         HttpCommunicator httpcon = new HttpCommunicator();
         var body = Map.of("gameName", gameName);
         var jsonBody = new Gson().toJson(body);
-        httpcon.run("game", "POST", authToken, jsonBody);
-        return 0;
+        var retjson = httpcon.run("game", "POST", authToken, jsonBody);
+        var id = new Gson().fromJson(retjson, idDeJuegos.class);
+        return id.getGameID();
     }
 
     public void joingame(String authToken, String playerColor, int gameID) throws Exception{
